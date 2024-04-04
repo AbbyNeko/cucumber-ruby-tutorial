9d654cb1739595465b6b7285ebe1c6dffb6636e2
Abigail Limpioso <alimpioso@gmail.com>
2024-04-04 11:34:58 -0400

    Update feature file name and added the recommended driver.quit instead of close

--- features/step_definitions/google_search_steps.rb
+++ features/step_definitions/google_search_steps.rb
@@ -2,7 +2,7 @@ require 'selenium-webdriver'
 
 Given(/^the user navigates to the Google page$/) do
   @driver = Selenium::WebDriver.for :chrome
-  @driver.get "www.google.com"
+  @driver.get "http://google.com"
 end
 
 When(/^the user searches for "(.*)"$/) do |keyword|
@@ -15,5 +15,5 @@ Then(/^the user is redirected to the results page for "(.*)"$/) do |keyword|
   wait = Selenium::WebDriver::Wait.new(timeout: 10)
   wait.until { @driver.title.downcase.start_with? keyword }
   puts "Page title is #{@driver.title}"
-  @driver.close
+  @driver.quit
 end
